64c3dc150e71ab2f1a1f93b5cf30893b372d3647
ryofu1210 <fujii@olivastyle.co.jp>
2019-06-27 11:00:05 +0900

    devise mail sender 変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'nomad-cafe-rails-app.herokuapp.com', port: 443 }
+  config.action_mailer.default_url_options = { host: 'https://nomad-cafe-rails-app.herokuapp.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
