6f48520c46ee23d54d8e57bcece5bc323a402e7f
ryofu1210 <fujii@olivastyle.co.jp>
2019-06-27 10:38:52 +0900

    sendgridのdomain情報変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'herokuapp.com',
+  :domain => 'nomad-cafe-rails-app.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
