fd7e1dc20dc7886bd0861b237667b87230bf9529
ryofu1210 <fujii@olivastyle.co.jp>
2019-07-01 14:58:16 +0900

    お気に入り登録ajax化テスト実行

--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Favorites', type: :system do
 
 
-  describe "お気に入り登録機能" do
+  describe "お気に入り登録機能" , js:true do
     before do
       @user = FactoryBot.create(:user)
       @store = FactoryBot.create(:store, name: "FAVO_STORE")
@@ -14,14 +14,16 @@ RSpec.describe 'Favorites', type: :system do
     it "お気に入り登録に成功し、お気に入りSTORE一覧に表示される" do
       expect{
         click_link nil, href: store_favorites_path(@store)
+        # save_and_open_page  
+        sleep(1)
       }.to change(Favorite.all, :count).by(1)
-
+      
       find('#FAVORITE').click
       expect(page).to have_content("FAVO_STORE")
     end
   end
 
-  describe "お気に入り削除機能" do
+  describe "お気に入り削除機能", js:true  do
     before do
       @user = FactoryBot.create(:user)
       @store = FactoryBot.create(:store, name: "FAVO_STORE")
@@ -34,6 +36,7 @@ RSpec.describe 'Favorites', type: :system do
       # save_and_open_page
       expect{
         click_link nil, href: store_favorite_path(store_id:@store.id, id:@user.id)
+        sleep(1)
       }.to change(Favorite.all, :count).by(-1)
 
       find('#FAVORITE').click
